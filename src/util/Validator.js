56713cabeb7692ea91ee849945214c559f3e7e3b
Fasko <30687020+fasko-web@users.noreply.github.com>
2021-09-30 14:33:17 -0400

    Create Validator.js

---
+++ src/util/Validator.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const Ajv = require('ajv');
+const Validator = new Ajv({
+  keywords: [
+    require('ajv-keywords/dist/definitions/transform')(),
+    require('ajv-keywords/dist/definitions/typeof')()],
+});
+
+Validator.addSchema(
+  {
+    type: 'object',
+    properties: {
+      name: { type: 'string', minLength: 1, maxLength: 32 },
+      description: { type: 'string', maxLength: 100 },
+      type: { type: 'string' },
+      guilds: {
+        type: 'array',
+        uniqueItems: true,
+        items: [{ type: 'string' }],
+      },
+      defaultPermission: { type: 'boolean' },
+      permissions: {
+        type: 'object',
+        properties: {
+          channel: {
+            type: 'array',
+            uniqueItems: true,
+            items: [{ type: 'string' }],
+          },
+          member: {
+            type: 'array',
+            uniqueItems: true,
+            items: [{ type: 'string' }],
+          },
+        },
+      },
+      execute: { typeof: 'function' },
+    },
+    required: ['name', 'execute'],
+    if: { properties: { type: { pattern: 'USER|MESSAGE' } } },
+    then: {
+      properties: {
+        name: { pattern: '([w](-| )?)*[w]+' },
+      },
+    },
+    else: {
+      properties: {
+        name: { transform: ['toLowerCase'], pattern: '[w-]+' },
+      },
+    },
+    additionalProperties: true,
+  },
+  'command',
+);
+
+Validator.isCommand = Validator.getSchema('command');
+
+Validator.isEvent = data => {
+  switch (true) {
+    case data:
+    case data.name && typeof data.name === 'string':
+    case data.execute && typeof data.execute === 'function':
+    case !data.once || (data.once && typeof data.once === 'boolean'): {
+      return true;
+    }
+    default: {
+      return false;
+    }
+  }
+};
+
+Validator.isInteraction = data => data?.execute && typeof data.execute === 'function';
+
+module.exports = Validator;
