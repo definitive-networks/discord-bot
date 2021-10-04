11d333334ede086d8aed55cecff5c2036bd7c556
Fasko <30687020+fasko-web@users.noreply.github.com>
2021-10-03 22:32:07 -0400

    Update BotClient.js

--- src/BotClient.js
+++ src/BotClient.js
@@ -2,6 +2,7 @@
 
 const { existsSync } = require('fs');
 const path = require('path');
+const { oneLine } = require('common-tags');
 const { Client } = require('discord.js');
 const CommandManager = require('./managers/CommandManager');
 const { Config, Util, Validator } = require('./util');
@@ -14,133 +15,159 @@ class BotClient extends Client {
 
     for (const [key, val] of Object.entries(this.config.directories)) {
       if (key === 'root') {
-        if (!val || !existsSync(val)) throw new Error('DIRECTORY_INVALID', key);
+        if (!val || !existsSync(val)) {
+          throw new Error(`Invalid directory was provided for ${key}: ${val}`);
+        }
         continue;
       }
       this.config.directories[key] =
-        val === false
-          ? false
+        typeof val === 'boolean' && val === false
+          ? val
           : path.join(
               this.config.directories.root,
-              ...(typeof val === 'string' ? val.split('/').filter(data => data) : [key]),
+              ...(typeof val === 'string' && val.length ? val.split('/').filter(data => data) : [key]),
             );
-      if (val !== false && !existsSync(this.config.directories[key])) {
-        throw new Error('DIRECTORY_INVALID', key);
+      if (this.config.directories[key] && !existsSync(this.config.directories[key])) {
+        throw new Error(`Invalid directory was provided for ${key}: ${this.config.directories[key]}`);
       }
     }
 
     if (this.config.database && this.config.database.enabled) {
       try {
         if (require.resolve('@prisma/client')) {
-          const { DatabaseManager } = require('./managers');
+          const { DatabaseManager } = require('./managers/DatabaseManager');
           this.database = new DatabaseManager(this, this.config.database.options);
         }
-      } catch (error) {
-        throw new Error(error);
+      } catch (err) {
+        throw new Error(err);
       }
     }
 
     this.commands = new CommandManager(this);
 
-    this.on('error', err => console.error(err));
+    if (process.env.NODE_ENV === 'development') {
+      this.on('error', err => console.error(err));
+      this.on('debug', message => console.log(message));
+      this.on('warn', info => console.log(info));
+      this.on('applicationCommandCreate', cmd => {
+        this.emit(
+          'debug',
+          `Created "${cmd.name}" command ${cmd.guild ? `in ${cmd.guild.name} (${cmd.guildId})` : 'globally'}.`,
+        );
+      });
+      this.on('applicationCommandDelete', cmd => {
+        this.emit(
+          'debug',
+          `Deleted "${cmd.name}" command ${cmd.guild ? `in ${cmd.guild.name} (${cmd.guildId})` : 'globally'}.`,
+        );
+      });
+      this.on('applicationCommandUpdate', ({ newCommand: cmd }) => {
+        this.emit(
+          'debug',
+          `Updated "${cmd.name}" command ${cmd.guild ? `in ${cmd.guild.name} (${cmd.guildId})` : 'globally'}.`,
+        );
+      });
+    }
 
-    if (process.env.NODE_ENV === 'development') this.initDebugEvents();
-
-    if (Boolean(this.config.initCommands) === true) this.initCommands();
-    if (Boolean(this.config.initEvents) === true) this.initEvents();
-    if (Boolean(this.config.initInteractions) === true) this.initInteractions();
-  }
-
-  get owners() {
-    if (!this.config.owners || !this.config.owners.length) return null;
-    if (typeof this.config.owners === 'string') return [this.config.owners];
-    const owners = [];
-    this.config.owners.forEach(owner => owners.push(owner));
-    return owners;
-  }
-
-  isOwner(user) {
-    const resolvedUser = this.users.resolve(user);
-    return this.owners?.length && this.owners.includes(resolvedUser);
-  }
-
-  initCommands(directory = this.config.directories.commands) {
-    require('require-all')({
-      dirname: directory,
-      resolve: data => this.commands.create(data),
-    });
-  }
-
-  initDebugEvents() {
-    this.on('applicationCommandCreate', command => {
-      this.emit(
-        'debug',
-        `Created "${command.name}" command (${command.id}) ${
-          command.guild ? `in ${command.guild.name} (${command.guildId})` : 'globally'
-        }.`,
-      );
-    });
-    this.on('applicationCommandDelete', command => {
-      this.emit(
-        'debug',
-        `Deleted "${command.name}" command ${
-          command.guild ? `in ${command.guild.name} (${command.guildId})` : 'globally'
-        }.`,
-      );
-    });
-    this.on('applicationCommandUpdate', ({ newCommand: command }) => {
-      this.emit(
-        'debug',
-        `Updated "${command.name}" command ${
-          command.guild ? `in ${command.guild.name} (${command.guildId})` : 'globally'
-        }.`,
-      );
-    });
-    this.on('debug', message => console.log(message));
-    this.on('warn', info => console.log(info));
-  }
-
-  initEvents(directory = this.config.directories.events) {
-    require('require-all')({
-      dirname: directory,
-      resolve: data => {
-        if (Validator.isEvent(data)) {
-          this[data.once ? 'once' : 'on'](data.name, async (...args) => {
-            await data.execute(...args, this).catch(err => this.emit('error', err));
-          });
-          return this.emit('debug', `Event created: ${data.name}`);
+    if (this.config.directories.commands) {
+      this.once('ready', async () => {
+        if (this.config.onReady.registerCommands) {
+          this.commands.registerFrom(this.config.directories.commands);
         }
-        return this.emit('warn', `Event failed to create: ${data}`);
-      },
-    });
-    this.once('ready', () => this.commands.sync().catch(err => this.emit('error', err)));
-    this.on('interactionCreate', interaction => {
+        if (this.config.onReady.sync) await this.commands.sync();
+      });
+    }
+
+    if (this.config.directories.interactions) {
+      require('require-all')({
+        dirname: this.config.directories.interactions,
+        resolve: data => {
+          const validator = Validator.isInteraction(data);
+          if (validator && !validator.error) {
+            this.on('interactionCreate', async interaction => {
+              if ('customId' in interaction && interaction.customId === (data.customId || data.name)) {
+                await data.execute(interaction, interaction.options, this);
+              }
+            });
+            this.emit('debug', `Interaction action created: ${data.customId || data.name}`);
+          } else {
+            this.emit('warn', `Invalid interaction action data: ${data}`);
+          }
+        },
+      });
+    }
+
+    if (this.config.directories.events) {
+      require('require-all')({
+        dirname: this.config.directories.events,
+        resolve: data => {
+          const validator = Validator.isEvent(data);
+          if (validator && !validator.error) {
+            this[data.once ? 'once' : 'on'](data.name, async (...args) => {
+              await data.execute(...args, this);
+            });
+            this.emit('debug', `Event action created: ${data.name}`);
+          } else {
+            this.emit('warn', `Invalid event action data: ${data}`);
+          }
+        },
+      });
+    }
+
+    this.on('interactionCreate', async interaction => {
       if (interaction.isCommand() || interaction.isContextMenu()) {
-        this.commands.handle(interaction).catch(err => this.emit('error', err));
+        const command = this.commands.getFromInteraction(interaction);
+
+        if (!command) {
+          this.emit(
+            'debug',
+            `Unknown command requested: ${interaction.commandName} (${interaction.commandId}, ${
+              interaction.inGuild() ? `guild ${interaction.guildId}` : `user ${interaction.user.id}`
+            })`,
+          );
+          if (this.config.unknownCommandResponse) {
+            await interaction.reply({
+              content: oneLine`
+                This command no longer exists.
+                It should no longer show up within the hour if it has been deleted.
+              `,
+              ephemeral: true,
+            });
+          }
+          return;
+        }
+
+        const hasPermission = command.hasPermission(interaction);
+        if (!hasPermission || typeof hasPermission === 'string') {
+          const data = { ...(typeof hasPermission === 'string' && { response: hasPermission }) };
+          await command.onBlock(interaction, 'permission', data);
+        }
+
+        const throttle = command.throttle(interaction.user.id);
+        if (throttle && command.throttler && throttle.usages + 1 > command.throttler.usages) {
+          const remaining = (throttle.start + command.throttler.duration * 1000 - Date.now()) / 1000;
+          const data = { throttle, remaining };
+          await command.onBlock(interaction, 'throttling', data);
+        }
+
+        if (throttle) throttle.usages++;
+        await command.execute(interaction, this.client);
       }
     });
   }
 
-  initInteractions(directory = this.config.directories.interactions) {
-    require('require-all')({
-      dirname: directory,
-      resolve: data => {
-        if (Validator.isInteraction(data)) {
-          this.on('interactionCreate', async interaction => {
-            await data.execute(interaction, interaction.options, this);
-          });
-        }
-      },
-    });
+  get owners() {
+    if (!this.config.owners || !this.config.owners.length) return null;
+    return typeof this.config.owners === 'string' ? [this.config.owners] : [...new Set(this.config.owners)];
   }
 
-  start(bot_token = this.token) {
-    this.login(bot_token);
+  isOwner(user) {
+    const resolvedUser = this.users.resolve(user);
+    return this.owners?.length && this.owners.includes(resolvedUser.id);
   }
 
-  async destroy() {
-    super.destroy();
-    if (this.database) await this.database.$disconnect();
+  start(bot_token) {
+    this.login(bot_token ?? this.token);
   }
 }
 
