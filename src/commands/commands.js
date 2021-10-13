48e5cf6b141b970d7f98e2537bb17c8c81d35e88
Fasko <30687020+fasko-web@users.noreply.github.com>
2021-10-13 04:24:47 -0400

    Update commands.js

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const { oneLine } = require('common-tags');
-const { MessageEmbed } = require('discord.js');
 
 module.exports = {
   name: 'commands',
@@ -14,11 +13,6 @@ module.exports = {
   args: (client, guildId) => [
     {
       type: 'SUB_COMMAND',
-      name: 'list',
-      description: 'Get a list of available commands.',
-    },
-    {
-      type: 'SUB_COMMAND',
       name: 'toggle',
       description: 'Toggles a command in the current guild for a specific role, user, or channel.',
       options: [
@@ -27,12 +21,15 @@ module.exports = {
           name: 'command',
           description: 'The command to enable.',
           required: true,
-          choices: client.commands.registry
-            .filter(cmd => !cmd.protected && cmd.type === 'CHAT_INPUT' && (cmd.isGlobal || cmd.ids.has(guildId)))
-            .map(cmd => ({
-              name: `${cmd.name}${cmd.isGlobal ? ` (Global)` : ''}`,
-              value: `${cmd.name}${cmd.isGlobal ? '_global' : ''}_cmd`,
-            })) || [
+          choices: (client.commands.registry.filter(
+            cmd => !cmd.protected && (cmd.guildIds?.includes(guildId) || cmd.guildIds?.includes('all')),
+          ).size &&
+            client.commands.registry
+              .filter(cmd => !cmd.protected && (cmd.guildIds?.includes(guildId) || cmd.guildIds?.includes('all')))
+              .map(cmd => ({
+                name: `${cmd.name} (${cmd.type.toLowerCase()}${cmd.type !== 'CHAT_INPUT' ? '_action' : ''})`,
+                value: `${cmd.name}_${cmd.type}_cmd`,
+              }))) || [
             {
               name: 'No Commands Found',
               value: 'no_commands_found',
@@ -59,40 +56,16 @@ module.exports = {
     },
   ],
   execute: async (interaction, client) => {
+    if (!interaction.inGuild()) return null;
     const subCommand = interaction.options.getSubcommand();
     switch (subCommand) {
-      case 'list': {
-        return interaction.reply({
-          embeds: [
-            new MessageEmbed()
-              .setTitle('List of Commands')
-              .setFields(
-                client.commands.registry
-                  .filter(
-                    cmd =>
-                      !cmd.guildIds ||
-                      (cmd.guildIds && (cmd.guildIds.includes('global') || cmd.guildIds.includes(interaction.guildId))),
-                  )
-                  .map(cmd => {
-                    const command = {
-                      name: cmd.type === 'CHAT_INPUT' ? `/${cmd.name}` : cmd.name,
-                      value: cmd.description || `${cmd.type}_ACTION`,
-                    };
-                    return command;
-                  }),
-              )
-              .setTimestamp(),
-          ],
-        });
-      }
       case 'toggle': {
         const selectedCommand = interaction.options.get('command');
         const registryCommand = client.commands.registry.find(
           cmd =>
             !cmd.protected &&
-            cmd.type === 'CHAT_INPUT' &&
-            `${cmd.name}${cmd.isGlobal ? '_global' : ''}_cmd` === selectedCommand.value &&
-            cmd.ids.has(!cmd.isGlobal ? interaction.guildId : 'global'),
+            `${cmd.name}_${cmd.type}_cmd` === selectedCommand.value &&
+            cmd.ids.has(interaction.guildId),
         );
         if (!registryCommand) {
           return interaction.reply({ content: 'Unable to find the selected command.', ephemeral: true });
@@ -107,14 +80,13 @@ module.exports = {
           targetMentionable?.value &&
           (targetMentionable.user || targetMentionable.member || targetMentionable.role)
         ) {
-          if (
-            client.application.commands.permissions.has({
-              ...(interaction.inGuild() && { guild: interaction.guildId }),
-              command: commandId,
-              permissionId: targetMentionable.user?.id || targetMentionable.member?.id || targetMentionable.role?.id,
-            })
-          ) {
-            registryCommand.updatePermissions(interaction.inGuild() && interaction.guildId, [
+          const hasPermission = await client.application.commands.permissions.has({
+            guild: interaction.guildId,
+            command: commandId,
+            permissionId: targetMentionable.user?.id || targetMentionable.member?.id || targetMentionable.role?.id,
+          });
+          if (hasPermission) {
+            registryCommand.updatePermissions(interaction.guildId, [
               {
                 id: targetMentionable.user?.id || targetMentionable.member?.id || targetMentionable.role?.id,
                 ...((targetMentionable.user || targetMentionable.member) && { type: 'USER' }),
@@ -132,7 +104,7 @@ module.exports = {
             });
           }
           const result = await client.application.commands.permissions[shouldEnable.value ? 'add' : 'remove']({
-            ...(interaction.inGuild() && { guild: interaction.guildId }),
+            guild: interaction.guildId,
             command: commandId,
             ...(shouldEnable.value && {
               permissions: [
@@ -149,7 +121,8 @@ module.exports = {
               : { roles: targetMentionable.role?.id }),
           });
           if (result) {
-            registryCommand.updatePermissions(interaction.inGuild() && interaction.guildId, result);
+            registryCommand.updatePermissions(interaction.guildId, result);
+            console.log(result);
             return interaction.reply({
               content: oneLine`
                 ${shouldEnable.value ? 'Enabled' : 'Disabled'} \`${registryCommand.name}\` command for 
