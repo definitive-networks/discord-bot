ce41c7a1a8047994e6ef643b7640d18114607836
Fasko <30687020+fasko-web@users.noreply.github.com>
2021-10-21 12:37:57 -0400

    Update Config.js

--- src/util/Config.js
+++ src/util/Config.js
@@ -15,22 +15,6 @@ class Config extends null {
         sync: true,
       },
       unknownCommandResponse: true,
-      database: {
-        enabled: false,
-        options:
-          process.env.NODE_ENV === 'developement'
-            ? {
-                log: [
-                  { level: 'query', emit: 'event' },
-                  { level: 'warn', emit: 'event' },
-                  { level: 'info', emit: 'event' },
-                  { level: 'error', emit: 'event' },
-                ],
-              }
-            : {
-                log: [{ level: 'error', emit: 'event' }],
-              },
-      },
       owners: [],
       intents: ['GUILDS', 'GUILD_MESSAGES', 'DIRECT_MESSAGES'],
       partials: ['CHANNEL'],
